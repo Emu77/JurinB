102fef2b20492d93464e30302cf0a27dcaa9068c
Emu77 <e.kronas@kronisoft.net>
2025-10-17 21:24:45 +0200

    fix: render homepage content

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,16 @@
 // app/page.jsx
 export default function Home() {
   return (
-    <main style={{ textAlign: "center", padding: "2rem", color: "#00ffcc" }}>
+    <main style={{
+      textAlign: "center",
+      paddingTop: "5rem",
+      fontFamily: "monospace",
+      backgroundColor: "black",
+      color: "#00FFCC",
+      height: "100vh"
+    }}>
       <h1>Moin Juri! 🚀</h1>
-      <p>Jetzt läuft dein App Router auf Vercel ✅</p>
+      <p>Jetzt läuft dein App Router endlich auf Vercel ✅</p>
     </main>
   );
 }
