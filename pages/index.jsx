c9c67b36b9c6f861a9a4217087245b2bc12bab09
Emu77 <e.kronas@kronisoft.net>
2025-10-17 21:43:09 +0200

    chore: remove temporary pages/index.jsx (use app router)

--- pages/index.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{padding:24, textAlign:'center', color:'#0ff'}}>
-      <h1>Moin Juri! (pages)</h1>
-      <p>Temp. Startseite – App Router folgt</p>
-    </main>
-  );
-}
